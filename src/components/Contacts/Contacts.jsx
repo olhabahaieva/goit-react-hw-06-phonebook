c5aecf416b2431149174d85900887184c4bd16f8
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-20 19:19:16 +0300

    created contacts Slice

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import css from './Contacts.module.css';
 import Section from 'components/Section';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contacts-reducer';
 
-const Contacts = ({ contacts, onDeleteContact }) => {
-  // eslint-disable-next-line
-  const [contactsState, setContacts] = useState([]);
+const Contacts = () => {
+ const contacts = useSelector(state=> state.contacts.contacts);
+ const dispatch = useDispatch();
 
   const handleDeleteClick = id => {
-    onDeleteContact(id);
-    handleDeleteContactFromLocalStorage(id);
-  };
-
-  const handleDeleteContactFromLocalStorage = id => {
-    const savedContacts = localStorage.getItem('PhonebookContacts');
-    if (savedContacts) {
-      const parsedContacts = JSON.parse(savedContacts);
-      const updatedContacts = parsedContacts.filter(
-        contact => contact.id !== id
-      );
-      setContacts(
-        localStorage.setItem(
-          'PhonebookContacts',
-          JSON.stringify(updatedContacts)
-        )
-      );
-    }
+   dispatch(deleteContact(id))
   };
 
   if (contacts.length === 0) {
     return null;
   }
+
+  // const handleDeleteContactFromLocalStorage = id => {
+  //   const savedContacts = localStorage.getItem('PhonebookContacts');
+  //   if (savedContacts) {
+  //     const parsedContacts = JSON.parse(savedContacts);
+  //     const updatedContacts = parsedContacts.filter(
+  //       contact => contact.id !== id
+  //     );
+  //     setContacts(
+  //       localStorage.setItem(
+  //         'PhonebookContacts',
+  //         JSON.stringify(updatedContacts)
+  //       )
+  //     );
+  //   }
+  // };
+
+
 
   return (
     <Section title="Contacts">
--- src/redux/contacts-reducer.js
+++ src/redux/contacts-reducer.js
@@ -1,18 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-    contacts: []
+const loadContactsFromLocalStorage = () =>{
+    const savedContacts = localStorage.getItem('PhonebookContacts');
+    if(savedContacts){
+        return JSON.parse(savedContacts)
+    }
+    return [];
+}
+const initialState = {
+    contacts: loadContactsFromLocalStorage()
 }
 
-const contactsSlice = createSlice({
+export const contactsSlice = createSlice({
     name: 'contacts',
     initialState,
     reducers: {
         addContacts: (state, action)=>{
-            state.contacts = action.payload;
+            state.contacts.push(action.payload)
+            localStorage.setItem('PhonebookContacts', JSON.stringify(state.contacts));
+        },
+        deleteContact: (state, action)=>{
+            state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
+            localStorage.setItem('PhonebookContacts', JSON.stringify(state.contacts))
         }
     }
 })
 
-export const {addContacts} = contactsSlice.actions;
+export const {addContacts, deleteContact} = contactsSlice.actions;
 export default contactsSlice.reducer;
