2c918e9ad7b756e4dc541056bc56b6476ce441dc
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-20 22:09:13 +0300

    replaced phonebook component with redux

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import css from './Phonebook.module.css';
 import Section from 'components/Section';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/phonebook-reducer';
 
-const Phonebook = ({ contacts, createContact }) => {
-  const [inputValue, setInputValue] = useState('');
-  const [inputNumber, setInputNumber] = useState('');
+function Phonebook () {
+  // const [inputValue, setInputValue] = useState('');
+  // const [inputNumber, setInputNumber] = useState('');
+
+  const contact = useSelector(state=> state.contact.contact)
+  const dispatch = useDispatch()
 
   const onChange = (e) => {
-    setInputValue(e.target.value);
+    dispatch(e.target.value);
   };
 
   const onNumberChange = (e) => {
-    setInputNumber(e.target.value);
+    dispatch(addContact(e.target.value));
   };
 
   const handleButtonClick = (e) => {
     e.preventDefault();
-
-    createContact(inputValue, inputNumber);
-    setInputValue('');
-    setInputNumber('');
+    dispatch(addContact(e.target.value));
+    // createContact(inputValue, inputNumber);
+    // setInputValue('');
+    // setInputNumber('');
   };
 
-  useEffect(() => {
-    if (contacts && contacts.length > 0) {
-      localStorage.setItem('PhonebookContacts', JSON.stringify(contacts));
-    }
-  }, [contacts]);
+  // if (contacts && contacts.length > 0) {
+  //   localStorage.setItem('PhonebookContacts', JSON.stringify(contacts));
+  // }
+
+  // useEffect(() => {
+  //   if (contacts && contacts.length > 0) {
+  //     localStorage.setItem('PhonebookContacts', JSON.stringify(contacts));
+  //   }
+  // }, [contacts]);
 
   return (
     <>
@@ -40,7 +49,7 @@ const Phonebook = ({ contacts, createContact }) => {
             <input
               type="text"
               name="name"
-              value={inputValue}
+              value={contact}
               onChange={onChange}
             />
             <label className={css.label} htmlFor="number">
@@ -49,7 +58,7 @@ const Phonebook = ({ contacts, createContact }) => {
             <input
               type="tel"
               name="number"
-              value={inputNumber}
+              value={contact}
               onChange={onNumberChange}
             />
 
@@ -74,3 +83,75 @@ Phonebook.propTypes = {
 };
 
 export default Phonebook;
+
+// const Phonebook = ({ contacts, createContact }) => {
+//   const [inputValue, setInputValue] = useState('');
+//   const [inputNumber, setInputNumber] = useState('');
+
+//   const onChange = (e) => {
+//     setInputValue(e.target.value);
+//   };
+
+//   const onNumberChange = (e) => {
+//     setInputNumber(e.target.value);
+//   };
+
+//   const handleButtonClick = (e) => {
+//     e.preventDefault();
+
+//     createContact(inputValue, inputNumber);
+//     setInputValue('');
+//     setInputNumber('');
+//   };
+
+//   useEffect(() => {
+//     if (contacts && contacts.length > 0) {
+//       localStorage.setItem('PhonebookContacts', JSON.stringify(contacts));
+//     }
+//   }, [contacts]);
+
+//   return (
+//     <>
+//       <Section title="Phonebook">
+//         <div className={css.phonebook}>
+//           <form className={css.form} action="">
+//             <label className={css.label} htmlFor="name">
+//               Name
+//             </label>
+//             <input
+//               type="text"
+//               name="name"
+//               value={inputValue}
+//               onChange={onChange}
+//             />
+//             <label className={css.label} htmlFor="number">
+//               Number
+//             </label>
+//             <input
+//               type="tel"
+//               name="number"
+//               value={inputNumber}
+//               onChange={onNumberChange}
+//             />
+
+//             <button
+//               onClick={handleButtonClick}
+//               className={css.button}
+//               name="submit"
+//               type="submit"
+//             >
+//               Add contact
+//             </button>
+//           </form>
+//         </div>
+//       </Section>
+//     </>
+//   );
+// };
+
+// Phonebook.propTypes = {
+//   contacts: PropTypes.array,
+//   createContact: PropTypes.func.isRequired,
+// };
+
+// export default Phonebook;
