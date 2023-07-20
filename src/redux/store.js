969e01e5bd82bc15b7913d3d17566da6636f8ec2
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-20 21:58:04 +0300

    added action to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from './filter-reducer';
 import contactsReducer from './contacts-reducer';
+import phonebookReducer from './phonebook-reducer';
 
 const store = configureStore({
   reducer: {
     filter: filterReducer,
     contacts: contactsReducer,
+    contact: phonebookReducer
   },
 });
 
