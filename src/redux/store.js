cbdf58d6639fc0dbd937fbf3cfa279c56cfdef00
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-20 18:23:27 +0300

    added contacts redux to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from './filter-reducer';
-
+import contactsReducer from './contacts-reducer';
 
 const store = configureStore({
   reducer: {
-    filter: filterReducer
-    }
-})
+    filter: filterReducer,
+    contacts: contactsReducer,
+  },
+});
 
 export default store;
