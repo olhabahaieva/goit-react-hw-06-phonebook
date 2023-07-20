a2d189511bef87a10a3794154c2a8e60a7ba9352
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-20 14:19:09 +0300

    basic setup for filter redux component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
+import { addFilter } from 'redux/filter/filter-reducer';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Filter = ({ onChange }) => {
+function Filter () {
+  const filter = useSelector(state=> state.filter);
+  const dispatch = useDispatch();
+
+  const onChange = (event) => {
+    const filterValue = event.target.value;
+    dispatch(addFilter(filterValue));
+  };
 
     return (
       <div className={css.filter}>
@@ -13,6 +22,7 @@ const Filter = ({ onChange }) => {
           onChange={onChange}
           className={css.filterInput}
           type="search"
+          value={filter}
         />
       </div>
     );
@@ -22,3 +32,25 @@ Filter.propTypes = {
   onChange: PropTypes.func
 }
 export default Filter;
+
+
+// const Filter = ({ onChange }) => {
+
+//   return (
+//     <div className={css.filter}>
+//       <label className={css.label} htmlFor="search">
+//         Find contacts by name
+//       </label>
+//       <input
+//         onChange={onChange}
+//         className={css.filterInput}
+//         type="search"
+//       />
+//     </div>
+//   );
+// }
+
+// Filter.propTypes = {
+// onChange: PropTypes.func
+// }
+// export default Filter;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import filterReducer from './filter/filter-reducer';
+
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    filterReducer,
+  },
 })
 
 export default store;
