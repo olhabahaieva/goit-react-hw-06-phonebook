686397836d328c1e06df850658f8c55a7757fcd9
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-25 14:04:14 +0300

    added redux persist store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { contactsSlice, filterSlice } from './phonebook-reducer';
-import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   contacts: contactsSlice.reducer,
   filter: filterSlice.reducer,
 });
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persister = persistStore(store);
+export default store;
