a083113ad442f2928d07b10d18d10f3f1fd4bf1d
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-25 13:54:41 +0300

    npm redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { contactsSlice, filterSlice } from './phonebook-reducer';
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
 
 const rootReducer = combineReducers({
   contacts: contactsSlice.reducer,
